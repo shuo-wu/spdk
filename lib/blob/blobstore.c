f57eef8c20d9c70a5dc8ab34f263c088f1b9116a
Damiano Cipriani <damiano.cipriani@suse.com>
2025-04-17 15:49:33 +0200

    blob: remove clusters checksums when data change
    When a blob with a child snapshot is deleted or when a snapshot is
    inflated or decoupled from its parent, snapshot's data could change.
    So we remove the snapshot clusters checksums, if present, to prevent the
    reading of obsolete values.
    
    Longhorn 10037
    
    Signed-off-by: Damiano Cipriani <damiano.cipriani@suse.com>

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -424,6 +424,16 @@ blob_back_bs_destroy(struct spdk_blob *blob)
 	blob->back_bs_dev = NULL;
 }
 
+static void
+bs_snapshot_free_clusters_checksums(struct spdk_blob *blob)
+{
+	if (blob->clusters_checksums) {
+		blob->num_clusters_checksums = 0;
+		free(blob->clusters_checksums);
+		blob->clusters_checksums = NULL;
+	}
+}
+
 struct blob_parent {
 	union {
 		struct {
@@ -7261,6 +7271,9 @@ bs_inflate_blob_done(struct spdk_clone_snapshot_ctx *ctx)
 		_blob->back_bs_dev = bs_create_zeroes_dev();
 	}
 
+	/* Blob data has changed, so I remove clusters checksums */
+	bs_snapshot_free_clusters_checksums(_blob);
+
 	/* Temporarily override md_ro flag for MD modification */
 	_blob->md_ro = false;
 	blob_remove_xattr(_blob, BLOB_SNAPSHOT, true);
@@ -8141,16 +8154,6 @@ struct snapshot_checksum_ctx {
 };
 
 static void
-bs_snapshot_free_clusters_checksums(struct spdk_blob *blob)
-{
-	if (blob->clusters_checksums) {
-		blob->num_clusters_checksums = 0;
-		free(blob->clusters_checksums);
-		blob->clusters_checksums = NULL;
-	}
-}
-
-static void
 bs_snapshot_checksum_cleanup_finish(void *cb_arg, int bserrno)
 {
 	struct snapshot_checksum_ctx *ctx = cb_arg;
@@ -8837,6 +8840,10 @@ delete_snapshot_sync_snapshot_xattr_cpl(void *cb_arg, int bserrno)
 			}
 		}
 	}
+
+	/* Clone data has changed, so I remove clusters checksums */
+	bs_snapshot_free_clusters_checksums(ctx->clone);
+
 	ctx->next_extent_page = 0;
 	delete_snapshot_update_extent_pages(ctx, 0);
 }
